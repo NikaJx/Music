4d9262d89857b283e8e2620a72bfc70cd53c1614
unknown <nika-jaxaia@mail.ru>
2022-02-12 20:07:47 +0300

    firts commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.playL');
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let music = document.createElement('div');
+
+        music = new Audio();
+        music.src = './music/Эндшпиль - Мой Babylon (HD 1080P).mp3';
+
+        music.play();
+    });
+
+  
+    const btAdd = document.querySelectorAll('.bt');
+    
+    btAdd.forEach((bt) => {
+        bt.addEventListener('click', () => {
+            alert('Hello Gali');
+        });
+    });
+});
