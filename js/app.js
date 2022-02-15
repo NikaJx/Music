f4c24e8ed21abdab6a05032b5b742e5705e749dc
unknown <nika-jaxaia@mail.ru>
2022-02-15 18:56:33 +0300

    commit two

--- js/app.js
+++ js/app.js
@@ -1,22 +1,71 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const btn = document.querySelector('.playL');
+   const btn1 = document.querySelector('.fav1'),
+         btn2 = document.querySelector('.fav2'),
+         btn3 = document.querySelector('.fav3'),
+         btn4 = document.querySelector('.fav4'),
+         btn5 = document.querySelector('.fav5');
 
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        let music = document.createElement('div');
+    // btn1.addEventListener('click', () => {
+    //     let music = document.createElement('div');
 
-        music = new Audio();
-        music.src = './music/Эндшпиль - Мой Babylon (HD 1080P).mp3';
+    //     music = new Audio();
+    //     music.src = './music/music.mp3';
 
-        music.play();
-    });
+    //     music.play();
+    // });
 
-  
-    const btAdd = document.querySelectorAll('.bt');
-    
-    btAdd.forEach((bt) => {
-        bt.addEventListener('click', () => {
-            alert('Hello Gali');
+    // btn2.addEventListener('click', () => {
+    //     let music = document.createElement('div');
+
+    //     music = new Audio();
+    //     music.src = './music/CryJaxx-CandyShop(feat. Junior Charles).mp3';
+
+    //     music.play();
+    // });
+
+    // btn3.addEventListener('click', () => {
+    //     let music = document.createElement('div');
+
+    //     music = new Audio();
+    //     music.src = './music/music3.mp3';
+
+    //     music.play();
+    // });
+
+    // btn4.addEventListener('click', () => {
+    //     let music = document.createElement('div');
+
+    //     music = new Audio();
+    //     music.src = './music/music4.mp3';
+
+    //     music.play();
+    // });
+
+    // btn5.addEventListener('click', () => {
+    //     let music = document.createElement('div');
+
+    //     music = new Audio();
+    //     music.src = './music/music5.mp3';
+
+    //     music.play();
+    // });
+
+
+    function musicPlay(btn, event, srcs) {
+        btn.addEventListener(event, () => {
+            let music = document.createElement('div');
+
+            music = new Audio();
+            music.src = srcs;
+
+            music.play();
         });
-    });
+    }
+
+    musicPlay(btn1, 'click', './music/music.mp3');
+    musicPlay(btn2, 'click', './music/CryJaxx-CandyShop(feat. Junior Charles).mp3');
+    musicPlay(btn3, 'click', './music/music3.mp3');
+    musicPlay(btn4, 'click', './music/music4.mp3');
+    musicPlay(btn5, 'click', './music/music5.mp3');
+
 });
