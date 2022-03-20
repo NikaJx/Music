598af4dd880cd73c748915cb9e4c88c42f8d0a1d
unknown <nika-jaxaia@mail.ru>
2022-03-20 20:41:08 +0300

    20.03.2022

--- js/app.js
+++ js/app.js
@@ -181,8 +181,193 @@
     }
 ];
 
+let alboms = [
+    {
+        id: 0,
+        alb: [
+            {
+                id: 0,
+                src: './img/Изображение 512.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019'
+            },
+            {
+                id: 1,
+                src: './img/Изображение 513.png',
+                title: 'Glastonbury 2000 (Live)',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 2,
+                src: './img/Изображение 514.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 3,
+                src: './img/Изображение 518.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 4,
+                src: './img/Изображение 525.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            }
+        ]
+    },
+    {
+        id: 1,
+        alb1: [
+            {
+                id: 0,
+                src: './img/6.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019'
+            },
+            {
+                id: 1,
+                src: './img/7.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 2,
+                src: './img/8.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 3,
+                src: './img/9.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            },
+            {
+                id: 4,
+                src: './img/10.png',
+                title: 'Welcome To The Blackout (Li...',
+                subtitle: 'Released: 30/11/2018'
+            }
+        ]
+    },
+    {
+        id: 2,
+        alb2: [
+            {
+                id: 0,
+                src: './img/single/1.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019'
+            },
+            {
+                id: 1,
+                src: './img/single/2.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019'
+            },
+            {
+                id: 2,
+                src: './img/single/3.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019'
+            },
+            {
+                id: 3,
+                src: './img/single/4.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 4,
+                src: './img/single/5.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            }
+        ]
+    },
+    {
+        id: 3,
+        alb3: [
+            {
+                id: 0,
+                src: './img/single/6.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 1,
+                src: './img/single/7.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 2,
+                src: './img/single/8.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 3,
+                src: './img/single/9.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 4,
+                src: './img/single/10.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            }
+        ]
+    },
+    {
+        id: 4,
+        alb4: [
+            {
+                id: 0,
+                src: './img/single/Изображение 538.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 1,
+                src: './img/single/Изображение 539.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 2,
+                src: './img/single/Изображение 540.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 3,
+                src: './img/single/Изображение 541.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            },
+            {
+                id: 4,
+                src: './img/single/Изображение 543.png',
+                title: 'Dance',
+                subtitle: 'Released: 15/02/2019',
+            }
+        ]
+    }
+
+];
+
 let indexSong = 0;
 
+/**
+ * This function changes the icon of the pause button to a play button or a play button to a pause
+ * button
+ * @param src - The path to the image you want to use.
+ * @param id - The id of the icon that was clicked.
+ */
 function pauseChangeIcon(src, id) {
     let pauseIconChange = document.querySelectorAll('.icons');
     pauseIconChange.forEach((iconChange, i) => {
@@ -194,6 +379,10 @@ function pauseChangeIcon(src, id) {
     });
 }
 
+/**
+ * The function takes in an id and changes the color of the row to actived
+ * @param id - The id of the song that is being played.
+ */
 function activeSong(id) {
     let trColor = document.querySelectorAll('tr');
     trColor.forEach((trC, i) => {
@@ -212,6 +401,11 @@ function activeSong(id) {
     });
 }
 
+/**
+ * The function takes in the song name and the song id and plays the song
+ * @param name - The name of the song to play.
+ * @param id - The id of the song you want to play.
+ */
 function playerMusic(name, id) {
     activeSong(id);
     let pauseIcon = document.querySelector('.pause');
@@ -227,7 +421,11 @@ function playerMusic(name, id) {
     indexSong = id;
 }
 
-function pauseSong() {
+/**
+ * Pause the song
+ * @param id - The id of the song that is currently playing.
+ */
+function pauseSong(id) {
     let pauseMusic = document.querySelector('.pause');
     let audio = document.getElementById('audio');
 
@@ -237,7 +435,8 @@ function pauseSong() {
         playerIcon.style.display = 'block';
 
         audio.pause();
-
+        activeSong(id);
+        pauseChangeIcon(id);
     });
 }
 
@@ -256,14 +455,12 @@ function nextMusic() {
     } else {
         playerMusic(musicArr[indexSong + 1].src, musicArr[indexSong + 1].id);
     }
-
-    
 }
 
 let progres = document.querySelector('.label');
 
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement;
+    const {duration, currentTime} = e.target;
     const progressPercent = (currentTime / duration) * 100;
 
     progres.style.width = `${progressPercent}%`;
@@ -291,13 +488,25 @@ function prevMusic() {
     }
 }
 
-function volume() {
-    let rangeValue = document.querySelector('#volume').value;
-    document.querySelector('.val').innerHTML = 'Value' + rangeValue;
-    console.log(rangeValue);
+function addFavorite(id) {
+    let favorite = document.querySelectorAll('.icon');
+    favorite.forEach((heart, i) => {
+        if(id === i) {
+            heart.setAttribute('src', './icons/Add to Favorite (web).png');
+        } else {
+            heart.setAttribute('src', './icons/Favorite.png');
+        }
+    });
 }
 
-volume();
+
+// function volume() {
+//     let rangeValue = document.querySelector('#volume').value;
+//     document.querySelector('.val').innerHTML = 'Value' + rangeValue;
+//     console.log(rangeValue);
+// }
+
+// volume();
 
 function showPlayer() {
     let playerMove = document.querySelector('.playerMusic');
@@ -318,7 +527,218 @@ function hidePlayer() {
 
 hidePlayer();
 
+let albomWrapper = document.querySelector('.albom-wrapper');
+let albomData = '';
+let fiveAlbom = 5;
 
+function addAlboms(arr, fiveAlbom) {
+    arr.slice(0, fiveAlbom).forEach((albom) => {
+        let al = `
+            <div class="albom">
+                <img src="${albom.src}">
+                <h3>${albom.title}</h3>
+                <p>${albom.subtitle}</p>
+            </div>
+        `;
+        albomData += al;
+    });
+}
+
+addAlboms(alboms[0].alb, fiveAlbom);
+
+albomWrapper.innerHTML = albomData;
+
+let albomWrapper1 = document.querySelector('.disnone');
+let albomData1 = '';
+let fiveAlbom1 = 5;
+
+function addAlboms1(arr, fiveAlbom) {
+    arr.slice(0, fiveAlbom).forEach((albom) => {
+        let al = `
+            <div class="albom">
+                <img src="${albom.src}">
+                <h3>${albom.title}</h3>
+                <p>${albom.subtitle}</p>
+            </div>
+        `;
+        albomData1 += al;
+    });
+}
+
+addAlboms1(alboms[1].alb1, fiveAlbom1);
+
+albomWrapper1.innerHTML = albomData1;
+
+let albomWrapper2 = document.querySelector('.wrap');
+let albomData2 = '';
+let fiveAlbom2 = 5;
+
+function addAlboms2(arr, fiveAlbom) {
+    arr.slice(0, fiveAlbom).forEach((albom) => {
+        let al = `
+            <div class="albom">
+                <img src="${albom.src}">
+                <h3>${albom.title}</h3>
+                <p>${albom.subtitle}</p>
+            </div>
+        `;
+        albomData2 += al;
+    });
+}
+
+
+addAlboms2(alboms[2].alb2, fiveAlbom2);
+
+albomWrapper2.innerHTML = albomData2;
+
+let albomWrapper3 = document.querySelector('.wrap3');
+let albomData3 = '';
+let fiveAlbom3 = 5;
+
+function addAlboms3(arr, fiveAlbom) {
+    arr.slice(0, fiveAlbom).forEach((albom) => {
+        let al = `
+            <div class="albom">
+                <img src="${albom.src}">
+                <h3>${albom.title}</h3>
+                <p>${albom.subtitle}</p>
+            </div>
+        `;
+        albomData3 += al;
+    });
+}
+
+addAlboms3(alboms[3].alb3, fiveAlbom3);
+
+albomWrapper3.innerHTML = albomData3;
+
+let albomWrapper4 = document.querySelector('.wrap4');
+let albomData4 = '';
+let fiveAlbom4 = 5;
+
+function addAlboms4(arr, fiveAlbom) {
+    arr.slice(0, fiveAlbom4).forEach((albom) => {
+        let al = `
+            <div class="albom">
+                <img src="${albom.src}">
+                <h3>${albom.title}</h3>
+                <p>${albom.subtitle}</p>
+            </div>
+        `;
+        albomData4 += al;
+    });
+}
+
+addAlboms4(alboms[4].alb4, fiveAlbom4);
+
+albomWrapper4.innerHTML = albomData4;
+
+
+let addBtnAlbom = document.querySelector('.bt-adds');
+let removeBtnAlbom = document.querySelector('.bt-remove');
+
+function removeAndAnd(block, none) {
+    addBtnAlbom.style.display = none;
+    removeBtnAlbom.style.display = block;
+}   
+
+function removeAndAnd2(block, none) {
+    addBtnAlbo2.style.display = none;
+    removeBtnAlbom2.style.display = block;
+}   
+
+function removeAndAnd3(block, none) {
+    addBtnAlbo3.style.display = none;
+    removeBtnAlbom3.style.display = block;
+}  
+
+addBtnAlbom.addEventListener('click', () => {
+    switch(fiveAlbom) {
+        case 5:
+            // albomWrapper.innerHTML = '';
+            // albomData = '';
+            addAlboms(alboms[0].alb, fiveAlbom += 5);
+            albomWrapper.innerHTML = albomData;
+            removeAndAnd('block', 'none');
+            console.log(fiveAlbom);
+
+            break;
+    }
+});
+
+removeBtnAlbom.addEventListener('click', () => {
+    switch(fiveAlbom) {
+        case 10: 
+        albomWrapper.innerHTML = '';
+        albomData = '';
+        addAlboms(alboms[0].alb, fiveAlbom -= 5);
+        albomWrapper.innerHTML = albomData;
+        removeAndAnd('none', 'block');
+    }
+});
+
+let addBtnAlbo2 = document.querySelector('.btn-add2');
+let removeBtnAlbom2 = document.querySelector('.bt-remove2');
+
+
+addBtnAlbo2.addEventListener('click', () => {
+    switch(fiveAlbom2) {
+        case 5:
+            // albomWrapper.innerHTML = '';
+            // albomData = '';
+            addAlboms2(alboms[2].alb2, fiveAlbom2 += 5);
+            albomWrapper2.innerHTML = albomData2;
+            removeAndAnd2('block', 'none');
+            console.log(fiveAlbom);
+
+            break;
+    }
+});
+
+removeBtnAlbom2.addEventListener('click', () => {
+    switch(fiveAlbom2) {
+        case 10: 
+        albomWrapper2.innerHTML = '';
+        albomData2 = '';
+        addAlboms2(alboms[0].alb, fiveAlbom2 -= 5);
+        albomWrapper2.innerHTML = albomData2;
+        removeAndAnd2('none', 'block');
+    }
+});
+
+
+let addBtnAlbo3 = document.querySelector('.btn-add3');
+let removeBtnAlbom3 = document.querySelector('.bt-remove3');
+
+addBtnAlbo3.addEventListener('click', () => {
+    switch(fiveAlbom4) {
+        case 5:
+            // albomWrapper.innerHTML = '';
+            // albomData = '';
+            addAlboms4(alboms[4].alb4, fiveAlbom4 += 5);
+            albomWrapper4.innerHTML = albomData4;
+            removeAndAnd3('block', 'none');
+            break;
+    }
+});
+
+removeBtnAlbom3.addEventListener('click', () => {
+    switch(fiveAlbom4) {
+        case 10: 
+        albomWrapper4.innerHTML = '';
+        albomData4 = '';
+        addAlboms4(alboms[4].alb4, fiveAlbom4 -= 5);
+        albomWrapper4.innerHTML = albomData4;
+        removeAndAnd3('none', 'block');
+    }
+});
+
+
+/**
+ * It adds the music to the table.
+ * @param arr - The array of objects that will be used to create the table.
+ * @param loadedMusicNumber - The number of music items to be loaded.
+ */
 let tbody = document.querySelector('#musicTable');
 var musicData = '';
 
@@ -340,7 +760,8 @@ function addMusic(arr, loadedMusicNumber) {
             <td>
                 <div class="align">
                     <img class="icons" src="./icons/Play.png" onclick="playerMusic('${item.src}', ${item.id})">
-                    <img class="icon" src="icon" src="./icons/AddToPlaylist(web).png">
+                    <img class="icon"  src="./icons/Add to Favorite (web).png" onclick="addFavorite('${item.id}')"
+                    >
                 </div>
             </td>
         </tr>`;
@@ -350,6 +771,7 @@ function addMusic(arr, loadedMusicNumber) {
 }
 
 addMusic(musicArr, loadedMusicNumber);
+
 
 function clearContent() {
     tbody.innerHTML = "";
@@ -364,6 +786,7 @@ function showAndHideBtn(block, none) {
     btnRemove.style.display = block;
 }
 
+/* The above code is adding 5 more songs to the table. */
 btnAdd.addEventListener('click', () => {
     switch(loadedMusicNumber) {
         case 5:
@@ -381,6 +804,7 @@ btnAdd.addEventListener('click', () => {
     }
 });
 
+/* The above code is adding a button to the page that will remove the last 5 songs from the page. */
 btnRemove.addEventListener('click', () => {
     switch(loadedMusicNumber) {
         case 15:
@@ -399,3 +823,33 @@ btnRemove.addEventListener('click', () => {
 });
 
 tbody.innerHTML = musicData;
+
+
+const popupBtn = document.querySelector('.popup_btn');
+const popupCont = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+
+popupBtn.addEventListener('click', () => {
+    popupCont.classList.add('open');
+}); 
+
+popupClose.addEventListener('click', () => {
+    popupCont.classList.remove('open');
+});
+
+
+const popBtnImg = document.querySelector('.imggs');
+const popupImg = document.querySelector('.imgs');
+const bgs = document.querySelector('.bgs');
+
+popBtnImg.addEventListener('click', () => {
+    popBtnImg.classList.add('imgg');
+    popupImg.classList.add('pop-img');
+});
+
+bgs.addEventListener('click', (e) => {
+    if(e.target === bgs) {
+        popBtnImg.classList.remove('imgg');
+        popupImg.classList.remove('pop-img');
+    }
+});
